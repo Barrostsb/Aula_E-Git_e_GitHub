8a07821595a2182a68f070b4e154c9958636192a
Thiago <Thiago@Thiago-PC>
2014-05-28 15:28:42 -0300

    Mudanca na classe main no eclipse

--- Controlede Versao/src/br/com/thiago/classes/Main.java
+++ Controlede Versao/src/br/com/thiago/classes/Main.java
@@ -1,12 +1,12 @@
 package br.com.thiago.classes;
 
 /** 
- * Mudanças feitas no Git Hub
+ * Mudancas feitas no Git Hub
  */
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Ola mundo...\nControle de Vers�a com EGit");
+		System.out.println("Ola mundo...\nControle de Versao com EGit");
 	}
 
 }
